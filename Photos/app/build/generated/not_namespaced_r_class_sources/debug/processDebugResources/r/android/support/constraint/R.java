6eac7270f040d7fdfcb67080f513300bbe177dfe
mdm717 <33270132+mdm717@users.noreply.github.com>
2018-11-29 18:18:35 -0500

    Opens the SD Card images

--- Photos/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ Photos/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f070023;
-        public static final int end = 0x7f07003c;
-        public static final int gone = 0x7f070043;
-        public static final int invisible = 0x7f07004e;
-        public static final int left = 0x7f070050;
-        public static final int packed = 0x7f070061;
-        public static final int parent = 0x7f070062;
-        public static final int percent = 0x7f070065;
-        public static final int right = 0x7f07006a;
-        public static final int spread = 0x7f070082;
-        public static final int spread_inside = 0x7f070083;
-        public static final int start = 0x7f070088;
-        public static final int top = 0x7f070097;
-        public static final int wrap = 0x7f07009d;
+        public static final int bottom = 0x7f070024;
+        public static final int end = 0x7f07003d;
+        public static final int gone = 0x7f070044;
+        public static final int invisible = 0x7f07004f;
+        public static final int left = 0x7f070051;
+        public static final int packed = 0x7f070062;
+        public static final int parent = 0x7f070063;
+        public static final int percent = 0x7f070066;
+        public static final int right = 0x7f07006b;
+        public static final int spread = 0x7f070083;
+        public static final int spread_inside = 0x7f070084;
+        public static final int start = 0x7f070089;
+        public static final int top = 0x7f070098;
+        public static final int wrap = 0x7f07009e;
     }
     public static final class styleable {
         private styleable() {}
--- Photos/app/src/main/java/com/craigmsirota/photos/AlbumView.java
+++ Photos/app/src/main/java/com/craigmsirota/photos/AlbumView.java
@@ -1,13 +1,49 @@
 package com.craigmsirota.photos;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridView;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+
 
 public class AlbumView extends AppCompatActivity {
+    private static final int READ_REQUEST_CODE = 42;
+    public static GridView gridView;
+    Button add, copy, paste, display, delete, move;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album_view);
+
+        gridView = (GridView) findViewById(R.id.GridView);
+        add = (Button) findViewById(R.id.add);
+        copy = (Button) findViewById(R.id.Copy);
+        paste = (Button) findViewById(R.id.paste);
+        display = (Button) findViewById(R.id.display);
+        delete = (Button) findViewById(R.id.delete);
+        move = (Button) findViewById(R.id.move);
+
+        add.setOnClickListener(new OnClickListener(){
+
+            public void onClick(View view){
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                startActivityForResult(intent, READ_REQUEST_CODE);
+
+            }
+            
+        });
     }
 }
